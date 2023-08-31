04275c2ee21d4868e8ec0327d8987241a1b1ae0c
Dang Nhat <88953246+dangnhatsimon@users.noreply.github.com>
2023-08-31 09:30:53 +0700

    Add files via upload

---
+++ Database Design/books.sql
@@ -0,0 +1,20 @@
+-- Add the book_id foreign key
+ALTER TABLE fact_booksales ADD CONSTRAINT sales_book
+    FOREIGN KEY (book_id) REFERENCES dim_book_star (book_id);
+    
+-- Add the time_id foreign key
+ALTER TABLE fact_booksales ADD CONSTRAINT sales_time
+    FOREIGN KEY (time_id) REFERENCES dim_time_star (time_id);
+    
+-- Add the store_id foreign key
+ALTER TABLE fact_booksales ADD CONSTRAINT sales_store
+    FOREIGN KEY (store_id) REFERENCES dim_store_star (store_id);
+
+-- Create dim_author with an author column
+CREATE TABLE dim_author (
+    author varchar(256)  NOT NULL
+);
+
+-- Insert authors into the new table
+INSERT INTO dim_author
+SELECT DISTINCT author FROM dim_book_star;
