23ae196d9a4bd71bdad54658f1ac22f9acb63882
Dang Nhat <88953246+dangnhatsimon@users.noreply.github.com>
2023-11-10 13:27:22 +0700

    Add files via upload

--- relational_db/union.sql
+++ relational_db/union.sql
@@ -21,3 +21,19 @@ UNION ALL
 SELECT country_code, year
 FROM populations
 ORDER BY code, year;
+
+
+-- Return all cities with the same name as a country
+SELECT name
+FROM cities
+INTERSECT
+SELECT name
+FROM countries;
+
+-- Return all cities that do not have the same name as a country
+SELECT name
+FROM cities
+EXCEPT
+SELECT name
+FROM countries
+ORDER BY name;
